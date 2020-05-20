61706172a72b627b3429f04116b6d3765b130f55
Helison Dornelas <helison.dornelas@hotmail.com>
2020-05-20 19:07:46 -0300

    Outros Entiites - segunda Migration

--- SalesWebMvc/Models/Departamento.cs
+++ SalesWebMvc/Models/Departamento.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http.Headers;
 
 namespace SalesWebMvc.Models
 {
@@ -9,5 +9,26 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public ICollection<Vendedor> Vendedores { get; set; } = new List<Vendedor>();
+
+        public Departamento()
+        {
+        }
+
+        public Departamento(int id, string nome)
+        {
+            Id = id;
+            Nome = nome;
+        }
+
+        public void AddVendedor(Vendedor vendendor)
+        {
+            Vendedores.Add(vendendor);
+        }
+
+        public double TotalDeVendas(DateTime inicial, DateTime final)
+        {
+            return Vendedores.Sum(Vendedor => Vendedor.TotalDeVendas(inicial, final));
+        }
     }
 }
---
+++ SalesWebMvc/Models/RegistroDeVendas.cs
@@ -0,0 +1,28 @@
+﻿using SalesWebMvc.Models.Enums;
+using System;
+
+
+namespace SalesWebMvc.Models
+{
+    public class RegistroDeVendas
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double valorVenda { get; set; }
+        public StatusDeVenda Status { get; set; }
+        public Vendedor Vendedor { get; set; }
+
+        public RegistroDeVendas()
+        {
+        }
+
+        public RegistroDeVendas(int id, DateTime date, double valorVenda, StatusDeVenda status, Vendedor vendedor)
+        {
+            Id = id;
+            Date = date;
+            this.valorVenda = valorVenda;
+            Status = status;
+            Vendedor = vendedor;
+        }
+    }
+}
